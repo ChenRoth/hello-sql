f1a81da5e23fbc6f5b824f4af4a589d2dfc9928c
Chen Roth <rothchen@gmail.com>
2019-05-14 20:31:34 +0300

    connect /movies to database and serve movies!

--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,22 @@
+const mysql = require('mysql2/promise');
 const express = require('express');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'cinemaville'
+    });
 
+    const [results, fields] = await connection.execute(`SELECT * FROM Movies`);
+    // line 13 is equivalent to this:
+    // const arr = await connection.execute(`SELECT * FROM Movies`);
+    // const results = arr[0];
+    // const fields = arr[1];
 
-router.get('/', (req, res) => {
-    res.send(['space jam', 'pulp fiction', 'spider-man'])
+    res.send(results);
 });
 
 
